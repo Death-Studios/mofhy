1bee610eeb49061f5a771ac6e0ac219f20a1d96c
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-15 15:27:59 +0530

    Update SQLHost.php

--- src/callback/SQLHost.php
+++ src/callback/SQLHost.php
@@ -1,2 +1,8 @@
 <?php
-$sql = mysqli_query($connect, "UPDATE `hosting_account` SET `account_sql` = '".$_POST['status']."' WHERE `account_username` = '".$_POST['username']."'");
+$status = htmlspecialchars($_POST['status']);
+$username = htmlspecialchars($_POST['username']);
+$sql = "UPDATE `hosting_account` SET `account_sql` = ? WHERE `account_username` = ?";
+$stmt = $connect->prepare($sql);
+$stmt -> bind_param("ss", $status, $username);
+$stmt -> execute();
+$stmt -> close();
