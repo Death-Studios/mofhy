c1c0e7e1d0dd4f9983273f7e98d4774917739b70
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-15 14:29:35 +0530

    add prep stmt

--- src/admin/function/cLogin.php
+++ src/admin/function/cLogin.php
@@ -2,9 +2,17 @@
 require __DIR__.'/Connect.php';
 require __DIR__.'/../handler/SessionHandler.php';
 if(isset($_GET['client_id'])){
-	$sql = mysqli_query($connect,"SELECT * FROM `hosting_clients` WHERE `hosting_client_key`='".mysqli_real_escape_string($connect, $_GET['client_id'])."'");
-	if(mysqli_num_rows($sql)>0){
-		$Data = mysqli_fetch_assoc($sql);
+	$client_id = mysqli_real_escape_string($connect, $_GET['client_id']);
+	$sql = "SELECT * FROM `hosting_clients` WHERE `hosting_client_key`= ?";
+	$stmt = $connect->prepare($sql);
+	$stmt -> bind_param("s", $client_id);
+	$stmt -> execute();
+	$result = $stmt->get_result();
+	$rows = $result->num_rows;
+	$fetch = $result->fetch_assoc();
+	$stmt -> close();
+	if($rows>0){
+		$Data = $fetch;
 		setcookie('LEFSESS', bin2hex($Data['hosting_client_email']), time()+30+86400, '/');
 			header('location: ../index.php');
 		}
