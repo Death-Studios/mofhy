538b4a14138792ce0e099247caf0cbc22b6ce571
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-15 15:19:29 +0530

    Update Activate.php

--- src/callback/Activate.php
+++ src/callback/Activate.php
@@ -1,43 +1,26 @@
 <?php
 require_once __DIR__.'/../handler/AreaHandler.php';
 require_once __DIR__.'/../handler/HostingHandler.php';
-$sql = mysqli_query($connect, "UPDATE `hosting_account` SET `account_status` = '1' WHERE `account_username` = '".$_POST['username']."'");
-$sql1 = mysqli_query($connect, "SELECT `account_for` FROM `hosting_account` WHERE `account_username` = '".$_POST['username']."'");
-$id = mysqli_fetch_assoc($sql1);
-$sql2 = mysqli_query($connect, "SELECT * FROM `hosting_clients` WHERE `hosting_client_key` = '".$id['account_for']."'");
-$ClientInfo = mysqli_fetch_assoc($sql2);
-$sql3 = mysqli_query($connect, "SELECT * FROM `hosting_account` WHERE `account_username` = '".$_POST['username']."'");
-$AccountInfo = mysqli_fetch_assoc($sql3);
+$one = 1;
+$sql = "UPDATE `hosting_account` SET `account_status` = ? WHERE `account_username` = ?";
+$stmt = $connect->prepare($sql);
+$stmt -> bind_param("is", $one, $_POST['username']);
+$stmt -> execute();
+$stmt -> close();
+$sql1 = "SELECT `account_for` FROM `hosting_account` WHERE `account_username` = ?";
+$stmt = $connect->prepare($sql1);
+$stmt -> bind_param("s", $_POST['username']);
+$stmt -> execute();
+$id = $stmt->get_result()->fetch_assoc();
+$stmt -> close();
+$sql2 = "SELECT * FROM `hosting_clients` WHERE `hosting_client_key` = ?";
+$stmt = $connect->prepare($sql2);
+$stmt -> bind_param("s", $id['account_for']);
+$stmt -> execute();
+$ClientInfo = $stmt->get_result()->fetch_assoc();
+$stmt -> close();
 $EmailTo = [['email' => $ClientInfo['hosting_client_email']]];
-$Body = "
-	<div class='container' style='margin-left:5%;margin-right:5%;margin-top:5%;'>
-	<div style='border-radius:1px solid grey;border-radius:5px;box-shadow:1px 1px 5px grey;padding:20px;font-family: Arial, Helvetica, sans-serif;'>
-	<h2 style='text-align:center;color:skyblue;'><b>New Account</b></h2><hr>
-	<h3>Hi ".$ClientInfo['hosting_client_fname'].",</h3><p>You have successfully created a new hosting account the details are given bellow.</p><br>
-	<b>cPanel Username: </b><span>".$AccountInfo['account_username']."</span><br>
-	<b>cPanel Password: </b><span>".$AccountInfo['account_password']."</span><br>
-	<b>cPanel URL: </b><span>".$HostingApi['api_cpanel_url']."</span><br><br>
-	<b>Main Domain: </b><span>".$AccountInfo['account_domain']."</span><br>
-	<b>Account Date: </b><span>".$AccountInfo['account_date']."</span><br>
-	<b>Server IP: </b><span>".$HostingApi['api_server_ip']."</span><br>
-	<b>Hosting Package: </b><span>".$HostingApi['api_package']."</span><br><br>
-	<b>FTP Username: </b><span>".$AccountInfo['account_username']."</span><br>
-	<b>FTP Password: </b><span>".$AccountInfo['account_password']."</span><br>
-	<b>FTP Hostname: </b><span>ftpupload.net</span><br>
-	<b>FTP Port:</b><span>21</span><br><br>
-	<b>MySQL Username: </b><span>".$AccountInfo['account_username']."</span><br>
-	<b>MySQL Password: </b><span>".$AccountInfo['account_password']."</span><br>
-	<b>MySQL Hostname: </b><span>".str_replace('cpanel', $AccountInfo['account_sql'], $HostingApi['api_cpanel_url'])."</span><br>
-	<b>MySQL Port: </b><span>3306</span><br><br>
-	<b>Nameserver 1: </b><span>".$HostingApi['api_ns_1']."</span><br>
-	<b>Nameserver 2: </b><span>".$HostingApi['api_ns_2']."</span>
-	<br>
-	<p>After login to your account you can use any service ❤</p>
-	<p>Regards ".$AreaInfo['area_name']."</p>
-	<hr>
-	<h4 style='text-align:center;'><b>Need our help?</b></h4><p style='text-align:center'><a href='".$AreaInfo['area_url']."newticket.php' style='color:skyblue;text-decoration:none;margin:0;'>We are here to help you out!</a></p>
-	</div>
-	</div>";
+$Body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html> <head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> <meta http-equiv="x-ua-compatible" content="ie=edge"> <meta name="x-apple-disable-message-reformatting"> <meta name="viewport" content="width=device-width, initial-scale=1"> <meta name="format-detection" content="telephone=no, date=no, address=no, email=no"> <style type="text/css"> body,table,td{font-family:Helvetica,Arial,sans-serif !important}.ExternalClass{width:100%}.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div{line-height:150%}a{text-decoration:none}*{color:inherit}a[x-apple-data-detectors],u+#body a,#MessageViewBody a{color:inherit;text-decoration:none;font-size:inherit;font-family:inherit;font-weight:inherit;line-height:inherit}img{-ms-interpolation-mode:bicubic}table:not([class^=s-]){font-family:Helvetica,Arial,sans-serif;mso-table-lspace:0pt;mso-table-rspace:0pt;border-spacing:0px;border-collapse:collapse}table:not([class^=s-]) td{border-spacing:0px;border-collapse:collapse}@media screen and (max-width: 600px){.w-full,.w-full>tbody>tr>td{width:100% !important}.p-lg-10:not(table),.p-lg-10:not(.btn)>tbody>tr>td,.p-lg-10.btn td a{padding:0 !important}.p-3:not(table),.p-3:not(.btn)>tbody>tr>td,.p-3.btn td a{padding:12px !important}.p-6:not(table),.p-6:not(.btn)>tbody>tr>td,.p-6.btn td a{padding:24px !important}*[class*=s-lg-]>tbody>tr>td{font-size:0 !important;line-height:0 !important;height:0 !important}.s-4>tbody>tr>td{font-size:16px !important;line-height:16px !important;height:16px !important}.s-6>tbody>tr>td{font-size:24px !important;line-height:24px !important;height:24px !important}.s-10>tbody>tr>td{font-size:40px !important;line-height:40px !important;height:40px !important}}</style> </head> <body class="bg-light" style="outline: 0; width: 100%; min-width: 100%; height: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: Helvetica, Arial, sans-serif; line-height: 24px; font-weight: normal; font-size: 16px; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; color: #000000; margin: 0; padding: 0; border-width: 0;" bgcolor="#f7fafc"> <table class="bg-light body" valign="top" role="presentation" border="0" cellpadding="0" cellspacing="0" style="outline: 0; width: 100%; min-width: 100%; height: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: Helvetica, Arial, sans-serif; line-height: 24px; font-weight: normal; font-size: 16px; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; color: #000000; margin: 0; padding: 0; border-width: 0;" bgcolor="#f7fafc"> <tbody> <tr> <td valign="top" style="line-height: 24px; font-size: 16px; margin: 0;" align="left" bgcolor="#f7fafc"> <table class="container" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;"> <tbody> <tr> <td align="center" style="line-height: 24px; font-size: 16px; margin: 0; padding: 0 16px;"> <table align="center" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%; max-width: 600px; margin: 0 auto;"> <tbody> <tr> <td style="line-height: 24px; font-size: 16px; margin: 0;" align="left"> <table class="s-10 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%"> <tbody> <tr> <td style="line-height: 40px; font-size: 40px; width: 100%; height: 40px; margin: 0;" align="left" width="100%" height="40"> &#160; </td></tr></tbody> </table> <table class="ax-center" role="presentation" align="center" border="0" cellpadding="0" cellspacing="0" style="margin: 0 auto;"> <tbody> <tr> <td style="line-height: 24px; font-size: 16px; margin: 0;" align="left"> <a href="https://'.$AreaInfo['area_url'].'" target="_blank" rel="noopener noreferrer"> <img src="https://assets.mofhy.tk/img/logo.png" style="height: auto; line-height: 100%; outline: none; text-decoration: none; display: block; border-style: none; border-width: 0;" alt="'.$AreaInfo['area_name'].' Logo"> </a> </td></tr></tbody> </table> <table class="s-10 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%"> <tbody> <tr> <td style="line-height: 40px; font-size: 40px; width: 100%; height: 40px; margin: 0;" align="left" width="100%" height="40"> &#160; </td></tr></tbody> </table> <table class="card p-6 p-lg-10 space-y-4" role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-radius: 6px; border-collapse: separate !important; width: 100%; overflow: hidden; border: 1px solid #e2e8f0;" bgcolor="#ffffff"> <tbody> <tr> <td style="line-height: 24px; font-size: 16px; width: 100%; margin: 0; padding: 40px;" align="left" bgcolor="#ffffff"> <h1 class="h3 fw-700" style="padding-top: 0; padding-bottom: 0; font-weight: 700 !important; vertical-align: baseline; font-size: 28px; line-height: 33.6px; margin: 0;" align="left"> Greetings, </h1> <table class="s-4 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%"> <tbody> <tr> <td style="line-height: 16px; font-size: 16px; width: 100%; height: 16px; margin: 0;" align="left" width="100%" height="16"> &#160; </td></tr></tbody> </table> <p class="" style="line-height: 24px; font-size: 16px; width: 100%; margin: 0;" align="left"> Your hosting account with username '.$_POST['username'].' has been activated successfully! Enjoy ...</p><table class="s-4 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%"> <tbody> <tr> <td style="line-height: 16px; font-size: 16px; width: 100%; height: 16px; margin: 0;" align="left" width="100%" height="16"> &#160; </td></tr></tbody> </table></td></tr></tbody> </table> <table class="s-6 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%"> <tbody> <tr> <td style="line-height: 24px; font-size: 24px; width: 100%; height: 24px; margin: 0;" align="left" width="100%" height="24"> &#160; </td></tr></tbody> </table> <div class="text-muted text-center" style="color: #718096;" align="center"> &#169; '.$cYear.' '.$AreaInfo['area_name'].'. All Rights Reserved. </div><table class="s-6 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%"> <tbody> <tr> <td style="line-height: 24px; font-size: 24px; width: 100%; height: 24px; margin: 0;" align="left" width="100%" height="24"> &#160; </td></tr></tbody> </table> </td></tr></tbody> </table> </td></tr></tbody> </table> </td></tr></tbody> </table> </body></html>';
 $Email = array(
 	'subject' => 'New Account',
 	'body' => $Body
