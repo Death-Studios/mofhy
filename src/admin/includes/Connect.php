7782fe728ab30c4db8a3ef5384f727dc5a78ea3a
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-15 16:54:13 +0530

    Update Connect.php

--- src/admin/includes/Connect.php
+++ src/admin/includes/Connect.php
@@ -13,19 +13,7 @@ if (!isset($connect))
         echo 'Connection not established';
     }
 }
-// Get User Gravatar Avatar
-function get_gravatar( $email, $s = 80, $d = 'mp', $r = 'g', $img = false, $atts = array() ) {
-	$url = 'https://www.gravatar.com/avatar/';
-	$url .= md5( strtolower( trim( $email ) ) );
-	$url .= "?s=$s&d=$d&r=$r";
-	if ( $img ) {
-	  $url = '<img src="' . $url . '"';
-	  foreach ( $atts as $key => $val )
-		$url .= ' ' . $key . '="' . $val . '"';
-	  $url .= ' />';
-	}
-	return $url;
-}
+
 // Get MOFHY Version
 function get_mofhy_version()
 {
