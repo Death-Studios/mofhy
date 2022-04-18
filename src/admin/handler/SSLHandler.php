12bde9b7731fd9c734883bd8c66b0a2cfbc997be
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-18 20:18:15 +0530

    Update SSLHandler.php

--- src/admin/handler/SSLHandler.php
+++ src/admin/handler/SSLHandler.php
@@ -1,5 +1,11 @@
 <?php
 require __DIR__.'/../../modules/GoGetSSL/GoGetSSLApi.php';
-$sql = mysqli_query($connect,"SELECT * FROM `hosting_ssl_api` WHERE `api_key`='FREESSL'");
-$SSLApi = mysqli_fetch_Assoc($sql);
+$api_key = "FREESSL";
+$sql = "SELECT * FROM `hosting_ssl_api` WHERE `api_key`= ?";
+$stmt = $connect->prepare($sql);
+$stmt -> bind_param("s", $api_key);
+$stmt -> execute();
+$result = $stmt->get_result();
+$SSLApi = $result->fetch_assoc();
+$stmt -> close();
 ?>
