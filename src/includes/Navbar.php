3b6a5ef101edaebaedc8c6cc7e145a7c3367ffad
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-18 20:12:59 +0530

    add dark/light mode toggle

--- src/includes/Navbar.php
+++ src/includes/Navbar.php
@@ -9,12 +9,31 @@
 			</a>
 		</h1>
 		<div class="navbar-nav flex-row order-md-last">
+			<?php if(strpos($_SERVER['REQUEST_URI'], "viewCertificate") || strpos($_SERVER['REQUEST_URI'], "viewAccount") !== true){?>
+				<a href="?theme=dark" class="nav-link px-0 hide-theme-dark" title="" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="Enable dark mode">
+					<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path></svg>
+				</a>
+				<a href="?theme=light" class="nav-link px-0 hide-theme-light" title="" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="Enable light mode">
+					<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="12" cy="12" r="4"></circle><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path></svg>
+				</a>
+			<?php }?>
 			<div class="nav-item dropdown">
-			<?php function get_gravatar($email,$s=80,$d='mp',$r='g',$img=false,$atts=array()){$url='https://www.gravatar.com/avatar/';$url.=md5(strtolower(trim($email)));$url.="?s=$s&d=$d&r=$r";if($img){$url='<img src="'.$url.'"';foreach($atts as $key=>$val)$url.=' '.$key.'="'.$val.'"';$url.=' />';}return $url;} ?>
+				<?php function get_gravatar($email, $s = 80, $d = 'mp', $r = 'g', $img = false, $atts = array())
+				{
+					$url = 'https://www.gravatar.com/avatar/';
+					$url .= md5(strtolower(trim($email)));
+					$url .= "?s=$s&d=$d&r=$r";
+					if ($img) {
+						$url = '<img src="' . $url . '"';
+						foreach ($atts as $key => $val) $url .= ' ' . $key . '="' . $val . '"';
+						$url .= ' />';
+					}
+					return $url;
+				} ?>
 				<a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-					<span class="avatar avatar-sm rounded-1" style="background-image: url(<?php echo get_gravatar($ClientInfo['hosting_client_email'])?>)"></span>
+					<span class="avatar avatar-sm rounded-1" style="background-image: url(<?php echo get_gravatar($ClientInfo['hosting_client_email']) ?>)"></span>
 					<div class="d-none d-xl-block ps-2">
-						<div><?php echo $ClientInfo['hosting_client_fname']." ".$ClientInfo['hosting_client_lname']; ?></div>
+						<div><?php echo $ClientInfo['hosting_client_fname'] . " " . $ClientInfo['hosting_client_lname']; ?></div>
 						<div class="mt-1 small text-muted"><?php echo $ClientInfo['hosting_client_email']; ?></div>
 					</div>
 				</a>
@@ -128,7 +147,7 @@
 						</a>
 					</li>
 					<li class="nav-item">
-						<a href="https://forum.mofh.cf/docs" class="nav-link" target="_blank">
+						<a href="https://forum.mofhy.xyz/docs" class="nav-link" target="_blank">
 							<span class="nav-link-icon d-md-none d-lg-inline-block">
 								<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-help" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 									<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -143,7 +162,7 @@
 						</a>
 					</li>
 					<li class="nav-item">
-						<a href="https://forum.mofh.cf" class="nav-link" target="_blank">
+						<a href="https://forum.mofhy.xyz" class="nav-link" target="_blank">
 							<span class="nav-link-icon d-md-none d-lg-inline-block">
 								<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-messages" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 									<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
