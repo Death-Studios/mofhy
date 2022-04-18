d722aab8b67c0dff788c133c2a1812f1935804af
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-18 20:15:07 +0530

    add fetch statements & show error

--- src/admin/viewclient.php
+++ src/admin/viewclient.php
@@ -8,8 +8,21 @@ if(isset($_GET['client_id'])){
 	require_once __DIR__.'/../handler/HostingHandler.php';
 	require_once __DIR__.'/../modules/UserInfo/UserInfo.php';
 	require_once __DIR__.'/includes/Navbar.php';
-	include __DIR__.'/template/ViewClient.php';
-	require_once __DIR__.'/includes/Footer.php';
+	// check if user exists
+	$hosting_client_key = mysqli_real_escape_string($connect, $_GET['client_id']);
+	$sql = "SELECT * FROM `hosting_clients` WHERE `hosting_client_key`= ?";
+	$stmt = $connect->prepare($sql);
+	$stmt->bind_param("s", $hosting_client_key);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$rows = $result->num_rows;
+	if($rows>0){
+		$ClientInfo = $result->fetch_assoc();
+		include __DIR__.'/template/ViewClient.php';
+		require_once __DIR__.'/includes/Footer.php';
+	} else{
+		include __DIR__.'/../template/503.php';
+	}
 }
 else{
 	$PageInfo = ['title'=>'Unathorized Access','rel'=>''];
