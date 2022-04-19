7b846e119376e0d65a2db8d58dae698752add4de
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-19 16:02:31 +0530

    Update validate.php

--- src/validate.php
+++ src/validate.php
@@ -5,5 +5,4 @@ require_once __DIR__.'/handler/AreaHandler.php';
 require_once __DIR__.'/includes/Header.php';
 require_once __DIR__.'/handler/CookieHandler.php';
 include __DIR__.'/template/Validate.php';
-require_once __DIR__.'/includes/Footer.php';
 ?>
