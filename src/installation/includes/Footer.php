cd62c17299b6415b4ac66f6f938e010ee57c10a3
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-19 16:07:05 +0530

    Update Footer.php

--- src/installation/includes/Footer.php
+++ src/installation/includes/Footer.php
@@ -1,6 +1,5 @@
 
-<script src="https://assets.mofhy.tk/js/tabler.min.js"></script>
-<script
-  src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+<script src="assets/js/tabler.min.js"></script>
+<script src="assets/js/jquery.min.js"></script>
 </body>
 </html>
