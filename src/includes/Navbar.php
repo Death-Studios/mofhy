9bfe996908b4f5ec7e7ed850d80970a8f04d18a3
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-20 21:12:06 +0530

    Update Navbar.php

--- src/includes/Navbar.php
+++ src/includes/Navbar.php
@@ -9,7 +9,7 @@
 			</a>
 		</h1>
 		<div class="navbar-nav flex-row order-md-last">
-			<?php if(strpos($_SERVER['REQUEST_URI'], "viewCertificate") || strpos($_SERVER['REQUEST_URI'], "viewAccount") !== true){?>
+			<?php if(strpos($_SERVER['REQUEST_URI'], "account_id") || strpos($_SERVER['REQUEST_URI'], "ssl_id") !== true){?>
 				<a href="?theme=dark" class="nav-link px-0 hide-theme-dark" title="" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="Enable dark mode">
 					<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path></svg>
 				</a>
