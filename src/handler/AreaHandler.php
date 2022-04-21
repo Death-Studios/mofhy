d09144f5ab585ca359860c71787d117e67bbbe00
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-21 18:28:10 +0530

    Update AreaHandler.php

--- src/handler/AreaHandler.php
+++ src/handler/AreaHandler.php
@@ -1,4 +1,7 @@
 <?php
+if (file_exists('installation/install.php')) {
+  header('location: installation/');
+}
 $area_key = "AREA";
 $sql = "SELECT * FROM `hosting_area` WHERE `area_key`= ?";
 $stmt = $connect->prepare($sql);
@@ -6,6 +9,7 @@ $stmt -> bind_param("s", $area_key);
 $stmt -> execute();
 $result = $stmt->get_result();
 $AreaInfo = $result->fetch_assoc();
+$stmt -> close();
 if($AreaInfo['area_status'] != 1){
 	header('location: maintaince');
 }
