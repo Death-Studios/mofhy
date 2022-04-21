3e2d07192224cb0d8d1647bbf83de84706dec429
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-21 18:46:46 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,3 @@
 <?php
-require_once __DIR__.'/includes/Connect.php';
-require_once __DIR__.'/handler/AreaHandler.php';
-require_once __DIR__.'/handler/CookieHandler.php';
 header('location: accounts');
 ?>
