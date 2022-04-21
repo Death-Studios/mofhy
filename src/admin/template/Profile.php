46064b54ed9a8a615bfacc78e719b9f769f50209
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-21 18:54:26 +0530

    remove mistake login function

--- src/admin/template/Profile.php
+++ src/admin/template/Profile.php
@@ -1,32 +1,58 @@
-<?php
-require __DIR__.'/Connect.php';
-require __DIR__.'/../handler/SessionHandler.php';
-require '../../function/CryptoLib.php';
-if(isset($_GET['client_id'])){
-	$client_id = mysqli_real_escape_string($connect, $_GET['client_id']);
-	$sql = "SELECT * FROM `hosting_clients` WHERE `hosting_client_key`= ?";
-	$stmt = $connect->prepare($sql);
-	$stmt -> bind_param("s", $client_id);
-	$stmt -> execute();
-	$result = $stmt->get_result();
-	$rows = $result->num_rows;
-	$fetch = $result->fetch_assoc();
-	$stmt -> close();
-	if($rows>0){
-		$Data = $fetch;
-		$string = \IcyApril\CryptoLib::randomString(32);
-		$sql1 = "UPDATE `hosting_clients` SET `hosting_client_cookie` = ? WHERE `hosting_client_email` = ?";
-		$stmt = $connect->prepare($sql1);
-		$stmt -> bind_param("ss", $string, $Data['hosting_client_email']);
-		$trigger = $stmt -> execute();
-		$stmt -> close();
-		if($trigger !== false){
-			setcookie('LEFSESS', $string, time()+30+86400, '/');
-			header('location: ../index.php');
-		}
-	}
-}
-else{
-	header('location: ../login.php');
-}
-?>
+<div class="page-wrapper">
+	<div class="container-xl">
+		<div class="page-header">
+
+			<div class="row">
+				<div class="col-12">
+				</div>
+			</div>
+			<div class="col-12">
+				<div class="card">
+					<div class="card-header">
+						<h3 class="card-title">Your Profile</h3>
+					</div>
+					<div class="card-body">
+						<div class="row row-0">
+							<div class="col-md-12">
+								<div class="table-responsive">
+									<table class="table table-bordered">
+										<tbody>
+											<tr>
+												<td><strong>Full Name</strong></td>
+												<td><?php echo $AdminInfo['admin_fname']." ".$AdminInfo['admin_lname']; ?></td>
+											</tr>
+											<tr>
+												<td><strong>Email Address</strong></td>
+												<td><?php echo $AdminInfo['admin_email']?></td>
+											</tr>
+											<tr>
+												<td><strong>Clientarea Name</strong></td>
+												<td><?php echo $AreaInfo['area_name']; ?></td>
+											</tr>
+											<tr>
+												<td><strong>Clientarea URL</strong></td>
+												<td><?php echo "https://".$_SERVER['HTTP_HOST']; ?></td>
+											</tr>
+											<tr>
+												<td><strong>Clientarea Email</strong></td>
+												<td><?php echo $AreaInfo['area_email']; ?></td>
+											</tr>
+											<tr>
+												<td><strong>Clientarea Status</strong></td>
+												<td><?php if($AreaInfo['area_status']==1){echo '<span class="badge bg-green">Operational</span>';}elseif($AreaInfo['area_status']==0){echo '<span class="badge bg-orange">Maintaince</span>';}?></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-footer">
+						<a href="profileSettings" class="btn btn-primary">
+							Edit Profile
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
